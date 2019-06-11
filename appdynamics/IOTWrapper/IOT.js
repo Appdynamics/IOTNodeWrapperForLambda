9fe97436d89437a79ec553db6c8ad47fb347eae8
EC2 Default User <ec2-user@ip-10-156-141-123.extnp.national.com.au>
2019-06-11 16:48:41 +1000

    reverting

--- appdynamics/IOTWrapper/IOT.js
+++ appdynamics/IOTWrapper/IOT.js
@@ -21,68 +21,53 @@ class IOT {
         }
         this.path = `/eumcollector/iot/v1/application/${this.config.appKey}/beacons`;
     }
-
     sendBeaconSync(beacon) {
-        const request = require('request');
         const options = {
             hostname: this.config.collector,
             port: 443,
             path: this.path,
-            method: 'POST',
-            proxy: 'http://forwardproxy.extnp.national.com.au:3128',
-            json: true,
-            body: JSON.stringify(beacon)
+            method: 'POST'
         };
-
         Logger_1.Logger.debug('IOT Beacon:');
         Logger_1.Logger.debug(JSON.stringify(beacon));
-
-        function callback(error, response, body) {
-            Logger_1.Logger.debug(response.statusCode);
-            if(response.statusCode == 200){
-                console.log('success')
-            } else {
-                console.log('error: '+ response.statusCode)
-                console.log(body)
-            }
-        }
-        Logger_1.Logger.debug('Running');
-        request(options, callback);
-        Logger_1.Logger.debug('Done');
+        const req = https.request(options, function (res) {
+            req.on('error', function (e) {
+                Logger_1.Logger.error('problem with request: ' + e.message);
+            });
+        });
+        const json = JSON.stringify(beacon);
+        req.write(`[${json}]`);
+        req.end();
     }
-
-    // sendBeaconAsync(beacon) {
-    //     return __awaiter(this, void 0, void 0, function* () {
-    //         const options = {
-    //             hostname: this.config.collector,
-    //             port: 443,
-    //             path: this.path,
-    //             method: 'POST'
-    //         };
-    //         Logger_1.Logger.debug('-=-=-=-=-=-=-=-  IOT Beacon -=-=-=-=-=-=-=-=');
-    //         Logger_1.Logger.debug(JSON.stringify(beacon));
-    //         // return new pending promise
-    //         return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
-    //             const req = https.request(options, function (res) {
-    //                 resolve('Success');
-    //             });
-    //             req.on('error', (err) => reject(err));
-    //             const json = JSON.stringify(beacon);
-    //             req.write(`[${json}]`);
-    //             req.end();
-    //         }));
-    //     });
-    // }
-    // sendBeacon(beacon) {
-    //     if (this.sync && this.isValid) {
-    //         this.sendBeaconSync(beacon);
-    //     }
-    //     else if (this.isValid) {
-    //         this.sendBeaconAsync(beacon).catch((err) => { Logger_1.Logger.error(err); });
-    //     }
-    // }
+    sendBeaconAsync(beacon) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const options = {
+                hostname: this.config.collector,
+                port: 443,
+                path: this.path,
+                method: 'POST'
+            };
+            Logger_1.Logger.debug('-=-=-=-=-=-=-=-  IOT Beacon -=-=-=-=-=-=-=-=');
+            Logger_1.Logger.debug(JSON.stringify(beacon));
+            // return new pending promise
+            return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+                const req = https.request(options, function (res) {
+                    resolve('Success');
+                });
+                req.on('error', (err) => reject(err));
+                const json = JSON.stringify(beacon);
+                req.write(`[${json}]`);
+                req.end();
+            }));
+        });
+    }
     sendBeacon(beacon) {
-        this.sendBeaconSync(beacon);
+        if (this.sync && this.isValid) {
+            this.sendBeaconSync(beacon);
+        }
+        else if (this.isValid) {
+            this.sendBeaconAsync(beacon).catch((err) => { Logger_1.Logger.error(err); });
+        }
     }
 }
 exports.IOT = IOT;
