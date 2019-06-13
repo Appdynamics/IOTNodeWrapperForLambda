18138327a032a2945053e5c3796d0a74493c8d39
EC2 Default User <ec2-user@ip-10-156-141-123.extnp.national.com.au>
2019-06-13 12:08:04 +1000

    hi

--- appdynamics/IOTWrapper/IOT.js
+++ appdynamics/IOTWrapper/IOT.js
@@ -22,7 +22,7 @@ class IOT {
             Logger_1.Logger.warn('Appkey is not set, no beacons will be sent.');
         }
         this.path = `/eumcollector/iot/v1/application/${this.config.appKey}/beacons`;
-        this.agent = new HttpsProxyAgent('http://fakeproxy:3128');
+        this.agent = new HttpsProxyAgent('http://forwardproxy:3128');
     }
     sendBeaconSync(beacon) {
         const options = {
